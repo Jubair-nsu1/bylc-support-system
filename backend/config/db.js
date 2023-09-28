4203c48a3f62dcd1ea3fc2ecfe22bc0e61a1c13e
Jubair Ahmed <62503637+Jubair-nsu1@users.noreply.github.com>
2023-09-28 12:00:31 +0600

    asd

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,8 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
       // 'mongodb://mongodb:27017/mern-proj',
-      process.env.MONGO_URI_LOCAL,
+      // process.env.MONGO_URI_LOCAL,
+      'mongodb+srv://jubair421:mango123@cluster0.ap8mvgi.mongodb.net/',
       // process.env.MONGO_URI_DOCKER,
     )
   } catch (error) {
